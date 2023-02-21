eff5256aa380c05a9166651c87b3ae5124f58b77
Wet-panties <caythlin1rez@gmail.com>
2023-02-21 18:08:48 +0500

    my first project

---
+++ decorator.py
@@ -0,0 +1,50 @@
+def decorator(function):
+    import time
+
+    def wrapper(*args, **kwargs):
+        print(f'Starting {function}')
+        start = time.time()
+        result = function(*args, **kwargs)
+        fin = time.time() - start + 0.003
+        print(f'{function} successfully ended in {fin}')
+        return result
+
+    return wrapper
+
+
+@decorator
+def helloeblo(name):
+    helloname = f'hello {name}'
+    return helloname
+
+
+test = helloeblo('EBLO')
+print(test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''def hellodeco():
+    def helloeblo():
+        print('привет сын шлюхи')
+
+    helloeblo()
+
+
+def newfuck(func):
+    print(f'Получена функция {func} Запускаю')
+    func()
+    print(f'{func} gotova!')
+
+
+hellodeco()
+newfuck(hellodeco)'''
