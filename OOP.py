5efa31d0cff87c20194b4df03e0ff18d75b08b76
Wet-panties <caythlin1rez@gmail.com>
2023-03-03 11:26:04 +0500

    закончил с базой ООП

--- OOP.py
+++ OOP.py
@@ -1,5 +1,8 @@
 class House:
 
+
+    roof = 'шифер'
+
     def __init__(self,
                  square: int,
                  floor: int,
@@ -9,27 +12,53 @@ class House:
         self.sq = square
         self.fl = floor
         self.w = wall
-        print('залупа построена')
+        print('дом построен')
 
     def build_balcony(self, i):
         '''вычитание первого значения из конструктора '''
         self.sq -= i
 
     def build_roof(self):
-    '''уебали крышу  '''
+        '''уебали крышу'''
+        print('уебали крышу материалом ' + self.roof)
 
 
-# указываем обязательные параметры, которые передаём в конструктор и сокращаем вызов функций из класса House в переменную houseP
-houseP = House(100, 5, 'Гиспокартон')
+# вывод переменной из класса, но нельзя вывести из конструктора до объявления houseP
+print(House.roof)
+# print(House.sq) # выдаст ошибку
 
-# выводим значение self.sq из класса
-print(f'площадь дома {houseP.sq}')
+houseP = House(100, 5, 'Кирпич ёбаный')
+print(houseP.roof)
+houseP.roof = 'Test'
+print(houseP.roof)
+                        # разные аттрибуты
+print(House.roof)
 
-# передаём переменную i
-houseP.build_balcony(10)
+# # указываем обязательные параметры, которые передаём в конструктор и сокращаем вызов функций
+# # из класса House в переменную houseP и обращаемся к классу только через неё
+# House.roof = 'залупа'
+# houseP = House(100, 5, 'Гиспокартон')
+# # print(houseP.sq) не выдаст ошибку
+# print(houseP.sq)
+# print(houseP.roof)
 
-# повторно выводим значение self.sq из класса
-print(f'площадь дома после запуска метода {houseP.sq}')
-print(type(houseP))
 
-# MyHome
+
+
+
+
+
+# # выводим значение self.sq из класса
+# print(f'площадь дома {houseP.sq}')
+#
+# # передаём переменную i
+# houseP.build_balcony(10)
+#
+# # повторно выводим значение self.sq из класса
+# print(f'площадь дома после запуска метода {houseP.sq}')
+# houseP.build_roof()
+#
+# print(type(houseP))
+#
+#
+# # MyHome
