c249908ad6f7baa49982db04bf0d2298f9ed0328
Wet-panties <caythlin1rez@gmail.com>
2023-05-02 10:34:32 +0500

    Объеденил проекты

--- none.py
+++ none.py
@@ -7,6 +7,8 @@ connection = props(user, password, ip, port)
 inet = connection.connect()
 '''если в переменную придут хоть какие-то данные, то  тип None сменится на любой другой'''
 
+
+
 if inet is None:
     print('Соси хуй')
 else:
